745698aa5d52d0c62e7c99d5f3cd40e11aecf70f
Kieran530 <118708257+Kieran530@users.noreply.github.com>
2023-06-26 18:00:18 -0700

    Service selections styled and Price option updated
    Added the functionality for service selections to change their style on select/disable. The Price also updates based on what is being selected.

--- users_app/static/js/app.js
+++ users_app/static/js/app.js
@@ -93,42 +93,101 @@
 $(document).ready(function() {
     $('#1-checkbox').on('change', function() {
       if ($(this).prop('checked')) {
+        // Disable the other checkbox
         $('#2-checkbox').prop({ disabled: true, checked: false });
+        // Change the background color of the parent label
+        $('#2-checkbox').parent('label').addClass('disabled')
       } else {
         $('#2-checkbox').prop('disabled', false);
+        $('#2-checkbox').parent('label').removeClass('disabled')
       }
     });
   
     $('#2-checkbox').on('change', function() {
       if ($(this).prop('checked')) {
         $('#1-checkbox').prop({ disabled: true, checked: false });
+        $('#1-checkbox').parent('label').addClass('disabled')
       } else {
         $('#1-checkbox').prop('disabled', false);
+        $('#1-checkbox').parent('label').removeClass('disabled')
       }
     });
   
     $('#3-checkbox').on('change', function() {
       if ($(this).prop('checked')) {
         $('#4-checkbox, #5-checkbox').prop({ disabled: true, checked: false });
+        $('#4-checkbox, #5-checkbox').parent('label').addClass('disabled')
       } else {
         $('#4-checkbox, #5-checkbox').prop('disabled', false);
+        $('#4-checkbox, #5-checkbox').parent('label').removeClass('disabled')
       }
     });
   
     $('#4-checkbox').on('change', function() {
         if ($(this).prop('checked')) {
           $('#3-checkbox, #5-checkbox').prop({ disabled: true, checked: false });
+          $('#3-checkbox, #5-checkbox').parent('label').addClass('disabled')
         } else {
           $('#3-checkbox, #5-checkbox').prop('disabled', false);
+          $('#3-checkbox, #5-checkbox').parent('label').removeClass('disabled')
         }
       });
 
       $('#5-checkbox').on('change', function() {
         if ($(this).prop('checked')) {
           $('#4-checkbox, #3-checkbox').prop({ disabled: true, checked: false });
+          $('#4-checkbox, #3-checkbox').parent('label').addClass('disabled')
         } else {
           $('#4-checkbox, #3-checkbox').prop('disabled', false);
+          $('#4-checkbox, #3-checkbox').parent('label').removeClass('disabled')
         }
       });
+
+});
+
+$(document).ready(function() {
+  // Calculate and update the total price
+  function calculateTotalPrice() {
+    var totalPrice = 0;
+    $('.checkbox-input:checked').each(function() {
+      console.log('this is reallllly working!')
+      var priceElement = $(this).closest('.service_buttons').find('.price');
+      console.log(priceElement)
+      var price = parseFloat(priceElement.text().replace('$', ''));
+      totalPrice += price;
+      
+    });
+    $('#total-price').text(totalPrice.toFixed(2));
+  }
+  console.log('this is working!!')
+  // Event listener for checkbox click
+  $('.checkbox-input').on('change', function() {
+    console.log('Checkbox changed');
+    calculateTotalPrice();
+  });
+
+  // Initial calculation of total price
+  calculateTotalPrice();
+  function applySelectedClass() {
+    $('.checkbox-input:checked').each(function() {
+      $(this).parent('label').addClass('selected');
+    });
+  }
+  
+  function removeSelectedClass() {
+    $('.checkbox-input').each(function() {
+      if (!$(this).prop('checked')) {
+        $(this).parent('label').removeClass('selected');
+      }
+    });
+  }
+
+  $('.checkbox-input').on('change', function() {
+    console.log('Checkbox changed');
+    calculateTotalPrice();
+    applySelectedClass();
+    removeSelectedClass();
+  });
+
 
 });
